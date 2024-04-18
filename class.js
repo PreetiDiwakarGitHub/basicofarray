1acf07a57e7572d9f5408ab130b748985574b050
PreetiDiwakarGitHub <p208016@gmail.com>
2024-04-18 10:53:35 +0530

    array question

--- class.js
+++ class.js
@@ -53,6 +53,31 @@
 //     }
 
 
+// question 4\\\\\\\\\\\\\\\\\\\
+
+// let arr=[1,2,3,4,5,6,7,8,9,10];
+// for(let i=0; i<arr.length; i=i+2){
+//     console.log(arr[i]);
+// }
+
+
+// question 5\\\\\\\\\\\\\\\\\\
+
+// let arr=[1,2,3,4,5,6,7,8,9];
+// for(let i=arr.length; i>0; i--){
+//     console.log(arr[i]);
+// }
+
+// question   6\\\\\\\\\\\\\\\\\
+
+// let arr=[1,2,3,4,5,6,7,8,9,11,12,13,14];
+// let sum=0;
+// for(let i=0; i<arr.length; i++){
+//      sum=sum+arr[i];
+   
+// }
+// console.log(sum);
+
 
 //  let arr=[7,9,14,16,19,21,28];
 //  arr.pop(4);
