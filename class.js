e0452fcf6ee11e5afc4bc50e0fb6389e114111a4
PreetiDiwakarGitHub <p208016@gmail.com>
2024-04-18 09:03:55 +0530

    array

---
+++ class.js
@@ -0,0 +1,82 @@
+// let arr=["preeti","priya","priyanka","punam"];
+// arr.length=3;
+// console.log(arr);
+// arr.length=5;
+// console.log(arr);
+// console.log(arr[4]);
+
+
+// let arr=[1,2,3,4,5];
+// for(let i=3; i<arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// let arr=[];
+// arr[21]="preeti";
+// console.log(arr.length);
+  
+
+
+// use only string
+// let arr = new Array("sheetal","preeti","priya");
+// console.log(arr);
+
+
+// question  1   \\\\\\\\\\\\\
+
+// let arr=[1,2,3,4,5,];
+// for(let i=0; i<arr.length; i++){
+//     if(arr[i]%2==0){
+//         console.log(arr[i]);
+//     }
+// }
+
+
+// question  2   \\\\\\\\\\\\\
+
+// let arr=[1,2,3,4,5,6,7,8,9];
+// for(let i=0; i<arr.length; i++){
+//     if(arr[i]%2!=0){
+//         console.log(arr[i])
+//     }
+// }
+
+
+// question 3 \\\\\\\\\\\\\\\\\\\
+
+
+// let arr=[7,9,14,16,19,21,28];
+//     for(let i=0; i<arr.length; i++){
+//         if(arr[i]%7==0 && arr[i]%2==0){
+//             console.log(arr[i]);
+//         }
+//     }
+
+
+
+//  let arr=[7,9,14,16,19,21,28];
+//  arr.pop(4);
+// console.log(arr);
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.pop(); 
+
+// let arr=[7,9,14,16,19,21,28];
+// arr.shift();
+// console.log(arr);
+
+
+// let arr=[6,7,9,1,2,3,4,];
+// arr.unshift(2,3);
+// console.log(arr);
+
+
+
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/PreetiDiwakarGitHub/basicofarray.git
+// git push -u origin main
