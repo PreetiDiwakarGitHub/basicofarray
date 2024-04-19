feba4cba109f91dca96b7d2ee72d260cb95396a1
PreetiDiwakarGitHub <p208016@gmail.com>
2024-04-19 10:50:28 +0530

    question

---
+++ class2.js
@@ -0,0 +1,87 @@
+// let arr = [[1,2,3],[3,4,5],[6,7,8]];
+// console.log(arr[0]);
+
+// let arr = [[1,2,3],[3,4,5],[6,7,8]];
+// console.log(arr[1][2]);
+
+// let arr=[1,2,3,4,5,6];
+// console.log(string(arr)==="1,2,3,4,5,6");    XXXXXXXXXXXXXXXXX
+
+
+// console.log([1,2]+[3,4]);      out put = 1,23,4
+
+// console.log([1,2,]+[3,4]) ;     
+
+// out put =  1,23,4
+
+
+// console.log([1,2]+[,1,3]);
+
+// out put = 1,2,1,3   
+
+// console.log([]+1);
+
+// out put = 1;
+
+// console.log([1]+1);
+
+// out put = 11
+
+// console.log([]==[]);
+
+// out put = false;
+
+// console.log(" " + 1);
+
+// console.log([2]==[2]);
+
+// out put = false;
+
+// console.log(" " == " ");
+
+// console.log("" == 0);
+
+
+// let arr = ["riya","priya","preeti"];
+// delete arr[1];
+// console.log(arr);
+
+// let arr = ["riya","priya","preeti"];
+// delete arr[1];
+// console.log(arr[1]);
+
+
+// let arr = ["riya","priya","preeti"];
+// arr.splice(1,1);
+// console.log(arr);
+
+
+// let arr = ["riya","priya","preeti","priyanka","punam"];
+// arr.splice(1,2);
+// console.log(arr);
+
+// let arr = ["riya","priya","preeti","priyanka","punam"];
+// arr.splice(1,1,"sita");
+// console.log(arr);
+
+
+// let arr = ["riya","priya","preeti","priyanka","punam"];
+//  arr.splice(1,0,"sita");
+//  console.log(arr);
+
+//  let arr = ["riya","priya","preeti","priyanka","punam"];
+//  arr.splice(1,0,"sita","sheetal");
+//  console.log(arr);
+
+// let arr = ["riya","priya","preeti","Arti","kopal"];
+// let removed = arr.splice(1,1);
+// console.log(removed);
+
+
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/PreetiDiwakarGitHub/jssplicemathods.git
+// git push -u origin main
